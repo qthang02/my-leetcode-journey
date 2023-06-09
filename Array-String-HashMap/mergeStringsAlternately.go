e7524d6b45cde49214c020dada05b019ee767fdb
qthang02 <nguyenquocthang909@gmail.com>
2023-06-09 20:22:31 +0700

    problems 1768: Merge Strings Alternately: ignore len(word1) and len(word2) equal 0 case

--- Array-String-HashMap/mergeStringsAlternately.go
+++ Array-String-HashMap/mergeStringsAlternately.go
@@ -3,13 +3,6 @@ package Array_String_HashMap
 func mergeAlternately(word1 string, word2 string) string {
 	rs := ""
 
-	if len(word1) == 0 {
-		return word2
-	}
-	if len(word2) == 0 {
-		return word1
-	}
-
 	if len(word1) == len(word2) {
 		for i := 0; i < len(word1); i++ {
 			rs += string(word1[i]) + string(word2[i])
