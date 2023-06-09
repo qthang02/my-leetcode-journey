2b7678170410ce5e6317aac4c09b83649acc7890
qthang02 <nguyenquocthang909@gmail.com>
2023-06-09 13:59:30 +0700

    problems 242: Valid Anagram: optimize memory

--- Array-String-HashMap/validAnagram.go
+++ Array-String-HashMap/validAnagram.go
@@ -3,35 +3,25 @@ package Array_String_HashMap
 import "strings"
 
 func isAnagram(s string, t string) bool {
-	if len(s) != len(t) {
-		return false
-	} else {
-		a := strings.Split(s, "")
-		b := strings.Split(t, "")
-		m := make(map[string]int)
+	str := strings.Split(s+t, "")
+	m := make(map[string]int)
 
-		for _, v := range a {
-			_, ok := m[v]
-			if !ok {
-				m[v] = 1
-			} else {
+	for i, v := range str {
+		_, ok := m[v]
+		if !ok {
+			m[v] = 1
+		} else {
+			if i < len(s) {
 				m[v]++
-			}
-		}
-
-		for _, v := range b {
-			_, ok := m[v]
-			if !ok {
-				return false
 			} else {
 				m[v]--
 			}
 		}
+	}
 
-		for _, v := range m {
-			if v > 0 {
-				return false
-			}
+	for _, v := range m {
+		if v > 0 {
+			return false
 		}
 	}
 
