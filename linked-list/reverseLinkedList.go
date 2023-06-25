0492bc504deb4aded53e0c4d105596d44a8d8a99
qthang02 <nguyenquocthang909@gmail.com>
2023-06-25 20:57:22 +0700

    problem 206. Reverse Linked List

---
+++ linked-list/reverseLinkedList.go
@@ -0,0 +1,14 @@
+package linkedlist
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+
+	return prev
+}
