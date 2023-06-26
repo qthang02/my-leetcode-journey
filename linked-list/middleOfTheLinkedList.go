a3c57f4eb0079d168946472db65fc771dceef1ba
qthang02 <nguyenquocthang909@gmail.com>
2023-06-26 15:10:36 +0700

    problem 876. Middle of the Linked List

---
+++ linked-list/middleOfTheLinkedList.go
@@ -0,0 +1,13 @@
+package linkedlist
+
+func middleNode(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
