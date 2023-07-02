e702296c53252680f7046ccac4384da2272c3bd2
qthang02 <nguyenquocthang909@gmail.com>
2023-07-02 22:49:48 +0700

    problem 128. Longest Consecutive Sequence

---
+++ Array-String-HashMap/longestConsecutiveSequence.go
@@ -0,0 +1,31 @@
+package Array_String_HashMap
+
+func longestConsecutive(nums []int) int {
+	set := map[int]bool{}
+
+	for _, num := range nums {
+		set[num] = true
+	}
+
+	res := 0
+
+	for _, num := range nums {
+		if set[num-1] {
+			continue
+		}
+
+		sequence := 1
+		temp := num + 1
+
+		for set[temp] {
+			sequence++
+			temp++
+		}
+
+		if sequence > res {
+			res = sequence
+		}
+	}
+
+	return res
+}
